09733294994b86adc06d7de373e853b5082f756d
Andrzej <gombkaa@gmail.com>
2022-04-07 12:50:33 +0200

    Add DataAccessLayer project

---
+++ Battleship.Data/DependencyInjection.cs
@@ -0,0 +1,17 @@
+﻿using Battleship.Core.Interfaces.Repositories;
+using Battleship.Data.Cache;
+using Battleship.Data.Cache.Interfaces;
+using Battleship.Data.Repositories;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Battleship.Data;
+
+public static class DependencyInjection
+{
+    public static void AddDataAccessLayer(this IServiceCollection services)
+    {
+        services.AddSingleton<IGameCache, GameCache>();
+        
+        services.AddTransient<IGameRepository, GameRepository>();
+    }
+}
